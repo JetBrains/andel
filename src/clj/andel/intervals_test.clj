25b5db65ff1a433ce09583e0e5466e02c465ec64
Pavel Sergeev <Pavel.Sergeev@jetbrains.com>
2017-07-31 17:51:10 +0300

    rewrite monoid; bulk insert

--- src/clj/andel/intervals_test.clj
+++ src/clj/andel/intervals_test.clj
@@ -2,105 +2,50 @@
   (:require [andel.intervals :refer :all]
             [clojure.test :refer :all]))
 
-(deftest create-and-destruct-tree
-  (is (let [ranges [[5 10] [11 13] [17 20]]]
-        (= ranges
-           (-> ranges
-               from-to->tree
-               tree->from-to)))))
-
 (deftest simple-insert
-  (is (= (-> (make-empty-interval-tree)
-             (add-interval {:from 5 :to 10})
-             (tree->from-to))
-         [[5 10] [plus-infinity plus-infinity]])))
+  (let [itree (-> (make-interval-tree)
+                  (add-intervals [{:from 5 :to 10}]))]
+    (is (= (tree->intervals itree) [{:from 5 :to 10}]))))
 
 (deftest multiple-insertitions
-  (is (= (-> (make-empty-interval-tree)
-             (add-interval {:from 5 :to 10})
-             (add-interval {:from 17 :to 20})
-             (add-interval {:from 11 :to 13})
-             (tree->from-to))
-         [[5 10] [11 13] [17 20] [plus-infinity plus-infinity]]))
-  (is (= (-> (make-empty-interval-tree)
-             (add-interval {:from 1 :to 10})
-             (add-interval {:from 9 :to 11})
-;             (add-interval {:from 8 :to 12})
-;             (add-interval {:from 3 :to 17})
-;             (add-interval {:from 2 :to 18})
-;             (add-interval {:from 1 :to 19})
-;             (add-interval {:from 7 :to 13})
-;             (add-interval {:from 6 :to 14})
-;             (add-interval {:from 5 :to 15})
-;             (add-interval {:from 4 :to 16})
-             (tree->from-to))
-         [[0 10] [1 19] [2 18] [3 17] [4 16] [5 15] [6 14] [7 13] [8 12] [9 11] [plus-infinity plus-infinity]])))
+  (let [intervals [{:from 1 :to 10}
+                   {:from 9 :to 11}
+                   {:from 8 :to 12}
+                   {:from 3 :to 17}
+                   {:from 2 :to 18}                   
+                   {:from 7 :to 13}
+                   {:from 6 :to 14}
+                   {:from 5 :to 15}
+                   {:from 4 :to 16}]
+        itree (reduce add-intervals (make-interval-tree) (map vector intervals))]
+    (is (= (tree->intervals itree) (sort-by :from intervals)))))
 
-(deftest insert-with-overlaping
-  (is (= (with-out-str
-           (-> (make-empty-interval-tree)
-             (add-interval {:from 1 :to 10})
-             (add-interval {:from 9 :to 11})
-             (tree->from-to)))
-         [[1 10] [9 11]])))
-
-
-(deftest insert-at-same-pos
-  (is (= (-> (make-empty-interval-tree)
-             (add-interval {:from 10 :to 12})
-             (add-interval {:from 10 :to 11})
-             (tree->from-to))
-      [[10 11] [10 12] [plus-infinity plus-infinity]]))
-  (is (= (-> (make-empty-interval-tree)
-             (add-interval {:from 10 :to 12})
-             (add-interval {:from 10 :to 11})
-             (add-interval {:from 10 :to 13})
-             (add-interval {:from 10 :to 18})
-             (add-interval {:from 10 :to 15})
-             (add-interval {:from 10 :to 17})
-             (tree->from-to))
-         ;; reversed to addition order
-         [[10 17] [10 15] [10 18] [10 13] [10 11] [10 12] [plus-infinity plus-infinity]])))
+(deftest bulk-insertitions
+  (let [intervals (sort-by :from  [{:from 1 :to 10}
+                                   {:from 9 :to 11}
+                                   {:from 8 :to 12}
+                                   {:from 3 :to 17}
+                                   {:from 2 :to 18}                   
+                                   {:from 7 :to 13}
+                                   {:from 6 :to 14}
+                                   {:from 5 :to 15}
+                                   {:from 4 :to 16}])
+        itree (add-intervals (make-interval-tree) intervals)]
+    (is (= (tree->intervals itree) intervals))))
 
 (deftest marker-query-test
-  (let [itree (-> (make-empty-interval-tree)
-                  (add-interval {:from 8 :to 18})
-                  (add-interval {:from 21 :to 30}))]
-    (is (= (query-markers itree 0 5) []))
-    (is (= (query-markers itree 5 8) [{:from 8 :to 18}]))
-    (is (= (query-markers itree 19 22) [{:from 21 :to 30}]))
-    (is (= (query-markers itree 10 25) [{:from 8 :to 18} {:from 21 :to 30}]))
-    (is (= (query-markers itree 2 35) [{:from 8 :to 18} {:from 21 :to 30}]))
-    (is (= (query-markers itree 35 50) []))))
+  (let [itree (-> (make-interval-tree)
+                  (add-intervals [{:from 8 :to 18}])
+                  (add-intervals [{:from 21 :to 30}]))]
+    (is (= [] (query-intervals itree 1 5)))
+    (is (= [{:from 8 :to 18}] (query-intervals itree 5 8)))
+    (is (= [{:from 21 :to 30}] (query-intervals itree 19 22)))
+    (is (= [{:from 8 :to 18} {:from 21 :to 30}] (query-intervals itree 10 25)))
+    (is (= [{:from 8 :to 18} {:from 21 :to 30}] (query-intervals itree 2 35)))
+    (is (= [] (query-intervals itree 35 50)))))
 
-(deftest marker-query-test
-  (let [itree (-> (make-empty-interval-tree)
-;                  (add-interval {:from 0 :to 10})
-                  (add-interval {:from 9 :to 11})
-                  (add-interval {:from 8 :to 12})
-                  (add-interval {:from 3 :to 17})
-                  (add-interval {:from 2 :to 18})
-                  (add-interval {:from 1 :to 19})
-                  (add-interval {:from 7 :to 13})
-                  (add-interval {:from 6 :to 14})
-                  (add-interval {:from 5 :to 15})
-                  (add-interval {:from 4 :to 16}))]
-    (query-markers itree 0 3)))
+(comment 
+  (run-tests)
 
-(run-tests)
+  (clojure.test/test-vars [#'andel.intervals-test/simple-insert]))
 
-
-(comment
-
-  (make-interval-tree)
-  
-  (let [tree (from-to->tree [[10 20] [50 100] [150 1000]])]
-    (-> tree
-        (scan-to-offset 25)))
-  
-  (-> (make-empty-interval-tree)
-      (add-interval {:from 5 :to 10})
-      (scan-to-offset 3)
-      )
-
-  )
--- src/cljc/andel/intervals.cljc
+++ src/cljc/andel/intervals.cljc
@@ -34,8 +34,9 @@
 
 (defn by-offset [offset]
   (fn [m]
-    (< offset (:rightest m))))
+    (< offset (+ (:offset m) (:rightest m)))))
 
+;; ondefined for non leaf nodes
 (defn from-to [loc]
   "Last node position relative to accumulated offset"
   (let [m (:metrics (tree/node loc))
@@ -45,7 +46,7 @@
     {:from from
      :to (+ from length)}))
 
-#_(defn fix-offset [loc offset']
+(defn fix-offset [loc offset']
   (tree/edit
    loc
    (fn [{:keys [metrics data] :as leaf}]
@@ -54,62 +55,35 @@
               :metrics fixed-interval
               :data fixed-interval)))))
 
-#_(defn from-to->intervals [is]
-  (first (reduce (fn [[res base] [from to]]
-                   [(conj res (Interval. (- from base)
-                                         (- to from)
-                                         nil))
-                    to])
-                 [[] 0]
-                 is)))
-
 (defn tree->intervals [tr]
   (loop [loc (zipper tr)
          acc []]
     (cond (tree/end? loc)
-          acc
-
-          (tree/leaf? loc)
-          (recur (tree/next loc)
-                 (let [data (get-in loc [0 :data])]
-                   (if data
-                     (conj acc data)
-                     acc)))
+          (->> acc
+               (drop 1)       ;; drop left sentinel
+               (drop-last 1)  ;; drop right sentinel
+               (vec))
+          
+          (tree/leaf? (tree/node loc))
+          (recur (tree/next loc) (conj acc (from-to loc)))
+          
           :else
-          (recur (tree/next loc)
-                 acc))))
-
-(defn intervals->from-to [is]
-  (first (reduce (fn [[res base] {:keys [offset len end-pos]}]
-                   [(conj res [(+ offset base)
-                               (+ offset base len)])
-                    (+ offset base len)])
-                 [[] 0]
-                 is)))
-
-(def tree->from-to
-  (comp intervals->from-to tree->intervals))
+          (recur (tree/next loc) acc))))
 
 (defn scan-to-offset [tr offset]
   (tree/scan (zipper tr)
              (by-offset offset)))
 
+(defn scan-to-end [tr offset]
+  (tree/scan (zipper tr)
+             (fn [m]
+               (< offset (+ (:offset m) (:length m))))))
+
 (defn make-leaf [offset length]
   (tree/make-leaf (map->Interval {:offset offset
                                   :length length
-                                  0})
+                                  :rightest 0})
                   tree-config))
-
-(defn insert-one [r-sibling-loc interval]
-  (let [r-offset-old (:offset (:metrics (tree/node r-sibling-loc)))
-        {r-from :from r-to :to} (from-to r-sibling-loc)
-        {i-from :from i-to :to} interval
-        i-len (- i-to i-from)
-        new-r-offset (- r-from i-to)
-        i-offset (- r-offset-old new-r-offset i-len)]
-    (-> r-sibling-loc
-        (tree/insert-left (make-leaf i-offset i-len))
-        (fix-offset new-r-offset))))
 
 (defn intervals->tree [intervals]
   (-> (map #(tree/make-leaf % tree-config) intervals)
@@ -126,46 +100,36 @@
                                     :length   0
                                     :rightest 0})]))
 
-(defn add-interval [itree {:keys [from to] :as interval}]
-  (let [loc (scan-to-offset itree from)]
-    (root (insert-one loc interval))))
+(defn insert-one [loc {:keys [from to] :as interval}]
+  (let [r-sibling-loc (tree/scan loc (by-offset from))
+        r-offset (-> r-sibling-loc tree/node :metrics :offset)
+        {r-from :from r-to :to} (from-to r-sibling-loc)
+        len (- to from)
+        new-r-offset (- r-from from)
+        offset (- r-offset new-r-offset)]
+    (-> r-sibling-loc
+        (tree/insert-left (make-leaf offset len))
+        (fix-offset new-r-offset))))
 
-(defn query-markers [itree from to]
-  (let [start-loc (scan-to-offset itree from)]
+(defn add-intervals [itree intervals]
+  (root (reduce insert-one (zipper itree) intervals)))
+
+;; add rightest right to monoid
+(defn query-intervals [itree from to]
+  (let [start-loc (scan-to-end itree from)]
     (loop [loc start-loc
            markers []]
-      (let [{loc-from :from loc-to :to}  (from-to loc)]
+      (let [{loc-from :from loc-to :to :as loc-from-to} (from-to loc)]
         (cond
+          (tree/node? (tree/node loc))
+          (recur (tree/next loc)
+                 markers)
+
           (< to loc-from)
           markers
-
-          (tree/leaf? loc)
-          (recur (tree/next loc)
-                 (conj markers {:from loc-from :to loc-to}))
-
+          
           :else
           (recur (tree/next loc)
-                  markers))))))
-
-(comment
-  (-> [[1 10] [3 6] [12 15]]
-      (tree->from-to))
-  )
-
-;(defn insert [it is]
-;  (->> is
-;       (reduce (fn [{:keys [loc updated-base]} i]
-;                 (if (some? updated-base)
-;                   (let [next-leaf (tree/next loc)
-;                         next-leaf-from-to (from-to next-leaf)
-;                         loc-fixed (if (<from-to next-leaf-from-to i)
-;                                     (fix-offset next-leaf
-;                                                 (- () updated-base)))]))
-;                (let [insert-loc (tree/scan loc (by-offset (:from i)))]
-;                  (insert-one insert-loc i)))
-;              {:loc (zipper it)
-;               :prev-to nil})
-;       (:loc)
-;       (root)))
-
-;(insert-bulk tr [{:from 3 :to 5} {:from 5 :to 6}])
+                 (if (< from loc-to)
+                   (conj markers loc-from-to)
+                   markers)))))))
