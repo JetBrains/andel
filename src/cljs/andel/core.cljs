395db6073a20ecf3d1e716fa026bc8dfb687c145
Pavel Sergeev <Pavel.Sergeev@jetbrains.com>
2017-08-30 20:20:10 +0300

    move marker tree benchmarks to separate file

--- src/cljs/andel/core.cljs
+++ src/cljs/andel/core.cljs
@@ -1,7 +1,6 @@
 (ns andel.core
     (:require [andel.lexer :as lexer]
               [andel.theme :as theme]
-              [andel.throttling :as throttling]
               [andel.controller :as contr]
               [andel.utils :as utils]
               [andel.intervals :as intervals]
@@ -16,6 +15,7 @@
               [clojure.set]
               [clojure.tools.reader.edn :as edn]
 
+              [andel.benchmarks]
               [create-react-class :as create-react-class]
               [react-dom :as react-dom])
     (:require-macros [reagent.interop :refer [$ $!]]
@@ -170,8 +170,8 @@
                      (map (fn [es]
                             {:pos (:pos (first es))
                              :add (set (map :add es))
-                             :remove (set (map :remove es))})))
-        res (:res (reduce (fn [{:keys [prev res styles]} {:keys [pos add remove]}]
+                             :remove (set (map :remove es))})))]
+    (:res (reduce (fn [{:keys [prev res styles]} {:keys [pos add remove]}]
                               {:prev pos
                                :styles (clojure.set/union (clojure.set/difference styles remove) add)
                                :res (push! res #js [:span {:class (->> styles
@@ -181,8 +181,7 @@
                             {:prev 0
                              :styles (:add (first events'))
                              :res #js [:pre {:class :line-text}]}
-                            (next events')))]
-    res))
+                            (next events')))))
 
 (defn render-background-markup [markup {:keys [height width spacing]}]
   (reduce (fn [res {:keys [from to background]}]
@@ -698,117 +697,3 @@
 (bind-function! "shift-down" contr/move-caret :down true)
 (bind-function! "esc" contr/drop-selection-on-esc)
 (bind-function! "enter" contr/on-enter)
-
-
-
-;; benchmarks
-
-(defn current-time! []
-  (.now js/Date))
-
-(defn text-tree-info [t]
-  (loop [acc {:nodes 0 :leafs 0}
-         loc (text/zipper t)]
-    (if (tree/end? loc)
-      (js/console.log (str "TEXT: " acc))
-      (if (tree/node? (tree/node loc))
-        (recur (update acc :nodes inc) (tree/next loc))
-        (recur (update acc :leafs inc) (tree/next loc))))))
-
-(defn intervals-tree-info [t]
-  (loop [acc {:nodes 0 :leafs 0}
-         loc (intervals/zipper t)]
-    (if (tree/end? loc)
-      (js/console.log (str "INTERVALS: " acc))
-      (if (tree/node? (tree/node loc))
-        (recur (update acc :nodes inc) (tree/next loc))
-        (recur (update acc :leafs inc) (tree/next loc))))))
-
-(defn bench [name f & {:keys [count] :or {count 10}}]
-  (let [start-time (current-time!)]
-    (js/console.log (str "START BENCH " name))
-    (mapv (fn [f] (f)) (repeat count f))
-    (let [end-time (current-time!)
-          total-time (- end-time start-time)]
-      (js/console.log (str "END BENCH: " name " "
-                            {:count count
-                             :total total-time
-                             :average (/ total-time count)})))))
-
-(defn bench-insert [markup]
-  (bench "TREE INSERT"
-         (fn []
-           (-> (intervals/make-interval-tree)
-               (intervals/add-intervals markup)))
-         :count 1))
-
-(defn bench-insert-base [markup]
-  (bench "BASE INSERT"
-   (fn []
-     (mapv (fn [m] (update m :from inc)) markup))
-   :count 100))
-
-(defn bench-query [markup]
-  (let [itree (-> (intervals/make-interval-tree)
-                  (intervals/add-intervals markup))]
-    (bench "TREE QUERY"
-           (fn []
-             (let [from (rand-int 160000)
-                   to (+ from 3200)]
-               (intervals/query-intervals itree {:from from :to to})))
-           :count 10000)))
-
-(defn play-query [model {:keys [from to]}]
-  (vec (filter #(intervals/intersect % {:from from :to to}) model)))
-
-(defn bench-query-base [markup]
-  (bench "QUERY BASE"
-         (fn []
-           (let [from (rand-int 160000)
-                 to (+ from 3200)]
-             (play-query markup {:from from :to to})))
-         :count 1000))
-
-(defn bench-type-in [markup]
-  (let [itree (-> (intervals/make-interval-tree)
-                  (intervals/add-intervals markup))]
-    (bench "TYPE-IN BENCH"
-           (fn []
-             (let [offset (rand-int 160000)
-                   size 1]
-               (intervals/type-in itree [offset size])))
-           :count 1000)))
-
-(defn bench-delete [markup]
-  (let [itree (-> (intervals/make-interval-tree)
-                  (intervals/add-intervals markup))]
-    (bench "DELETE BENCH"
-           (fn []
-             (let [offset (rand-int 160000)
-                   size 1]
-               (intervals/delete-range itree [offset size])))
-           :count 1)))
-
-(defn bench-editing [markup]
-  (let [itree (-> (intervals/make-interval-tree)
-                  (intervals/add-intervals markup))]
-    (bench "TREE EDITING"
-           (fn []
-             (let [cmd (rand-nth [:insert :delete])]
-               (case cmd
-                 :insert ))))))
-
-(bind-function! "ctrl-b" (fn [s]
-                           (let [markup (:raw-markers s)
-                                 interval-tree (get-in s [:document :markup])
-                                 text-tree (get-in s [:document :text])]
-                             #_(text-tree-info text-tree)
-                             (intervals-tree-info interval-tree)
-                             #_(bench-insert markup)
-                             #_(bench-insert-base markup)
-                             #_(bench-query markup)
-                             #_(bench-query-base markup)
-                             #_(bench-type-in markup)
-                             #_(bench-delete markup))
-                           (js/alert "BENCH DONE")
-                           s))
