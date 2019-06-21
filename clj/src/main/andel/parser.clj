9c4f67cbe0cb8a21bdacacd0677ab802e643be33
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2019-06-21 15:17:25 +0200

    add andel.jsitter module; implement jsitter.api.Text for andel.Text

--- clj/src/main/andel/parser.clj
+++ clj/src/main/andel/parser.clj
@@ -7,7 +7,7 @@
 (defn s-expr [^Zipper z]
   (let [node-type (.getNodeType z)]
     (if (instance? Terminal node-type)
-      (symbol (.getName node-type))
+      (.getName node-type)
       (lazy-seq (let [child (.down z)]
                   (apply list (symbol (.getName node-type))
                          (map s-expr (take-while some? (iterate #(.right ^Zipper %) child)))))))))
@@ -19,25 +19,9 @@
   (def cobra-go-str (slurp "/Users/jetzajac/cobra.go"))
   (def cobra-go-str (slurp "/Users/jetzajac/Projects/jsitter/testData/router_go"))
 
-  (def t (andel.Text/makeText cobra-go-str (andel.Text$TextOps. 32 256)))
+  (def text (andel.Text/makeText cobra-go-str))
 
-
-  (def chunk-bytes-size (* 1024 1024))
-  (def utf16-le kotlin.text.Charsets/UTF_16LE)
-
-  (def jsitter-text
-    (reify jsitter.api.Text
-         (read [_ start-byte buffer]
-               (let [start-char (quot start-byte 2)
-                     loc (text/scan-to-char-offset (text/zipper t) start-char)
-                     text-chars-count (text/chars-count t)]
-                 (andel.Text/reduceText loc (min (quot chunk-bytes-size 2) (- text-chars-count start-char))
-                                        nil
-                                        (reify andel.Text$TextReducer
-                                               (rf [_ _ str start end]
-                                                   (let [bytes (.getBytes ^String str utf16-le)]
-                                                     (.put buffer bytes (* 2 start) (* 2 (- end start)))))))))
-         (getEncoding [_] jsitter.api.Encoding/UTF16)))
+  (def jsitter-text (andel.JSitterText. text))
 
   (def source_file (NodeType. "source_file"))
 
@@ -47,10 +31,8 @@
 
   (def parser (.parser golang source_file))
 
-  (require )
-
   (time
-   (def parse-res (.parse parser jsitter-text [])))
+   (def parse-res (.parse parser jsitter-text nil)))
 
   (def tree (.getTree parse-res))
   (def root (.zipper tree))
@@ -58,7 +40,5 @@
   (clojure.pprint/pprint (s-expr root))
 
   (.getByteSize tree)
-
-
 
   )
