bca0cf7c98a9da7d2a25b37a9e374262f7a03ca5
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2020-03-09 17:25:38 +0100

    move stuff out of meta map

--- andel.core/src/main/java/andel/Composite.java
+++ andel.core/src/main/java/andel/Composite.java
@@ -8,39 +8,62 @@ import java.util.Objects;
 import java.util.function.Function;
 
 public class Composite {
-  public static final Attr<List<Log.Entry>> LOG = new Attr<>("LOG");
-  public static final Attr<Object> EDIT_AUTHOR = new Attr<>("AUTHOR");
-
   public final Map<Object, Component> components;
+  public final Log log;
+  public final Object editsAuthor;
   public final Text text;
   public final Map meta;
 
-  public Composite(Map<Object, Component> components, Text text, Map meta) {
+  public Composite(Map<Object, Component> components, Text text, Log log, Object editsAuthor, Map meta) {
     this.components = components;
     this.text = text;
     this.meta = meta;
+    this.log = log;
+    this.editsAuthor = editsAuthor;
   }
 
   public Composite edit(Edit edit) {
     Text textAfter = this.text.edit(edit);
     Map<Object, Component> components = this.components
       .mapValues((key, component) -> component.edit(this.text, textAfter, edit));
-    return new Composite(components, textAfter, this.meta);
+    return new Composite(components,
+                         textAfter,
+                         log,
+                         editsAuthor,
+                         this.meta);
   }
 
   public Composite assoc(Object key, Component component) {
     return new Composite(this.components.put(key, component),
                          this.text,
+                         this.log,
+                         this.editsAuthor,
                          this.meta);
   }
 
   @SuppressWarnings("unchecked")
   public Composite log(Op op, Object arg, Edit edit) {
-    return this.varyMeta((m) -> m.update(LOG, l -> (l == null ? new Log() : (Log)l).add(op, arg, edit, meta.get(EDIT_AUTHOR, null))));
+    return new Composite(this.components,
+                         this.text,
+                         this.log.add(op, arg, edit, editsAuthor),
+                         this.editsAuthor,
+                         this.meta);
+  }
+
+  public Composite withLog(Log log) {
+    return new Composite(this.components,
+                         this.text,
+                         log,
+                         this.editsAuthor,
+                         this.meta);
   }
 
   public Composite varyMeta(Function<Map, Map> f) {
-    return new Composite(this.components, this.text, f.apply(this.meta));
+    return new Composite(this.components,
+                         this.text,
+                         this.log,
+                         this.editsAuthor,
+                         f.apply(this.meta));
   }
 
   @Override
@@ -48,13 +71,15 @@ public class Composite {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Composite composite = (Composite)o;
-    return Objects.equals(components, composite.components) &&
-           Objects.equals(text, composite.text) &&
-           Objects.equals(meta, composite.meta);
+    return Objects.equals(this.components, composite.components) &&
+           Objects.equals(this.text, composite.text) &&
+           Objects.equals(this.meta, composite.meta) &&
+           Objects.equals(this.log, composite.log) &&
+           Objects.equals(this.editsAuthor, composite.editsAuthor);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(components, text, meta);
+    return Objects.hash(components, text, meta, log, editsAuthor);
   }
 }
